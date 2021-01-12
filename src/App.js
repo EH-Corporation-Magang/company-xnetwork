9b3bdb808ba1e9149c39abed64ad79cc8b83a3ba
Muhammad Farras Jibran <farasjibran@gmail.com>
2021-01-12 10:35:28 +0700

    update home screen

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             <div className="container">
               <nav className="navbar navbar-expand-lg navbar-light foi-navbar">
                 <a className="navbar-brand" href="index.html">
-                  <img style={{ width: '20%' }} src="images/logo.png" alt="Xnetwork" />
+                  <img src="images/logo.png" alt="Xnetwork" />
                 </a>
                 <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon" />
@@ -28,29 +28,26 @@ function App() {
                 <div className="collapse navbar-collapse" id="collapsibleNavId">
                   <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item active">
-                      <a className="nav-link" href="index.html" style={{ color: 'white' }}>Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="about.html" style={{ color: 'white' }}>About</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="features.html" style={{ color: 'white' }}>Features</a>
+                      <a className="nav-link" href="index.html" >Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item dropdown">
-                      <a className="nav-link dropdown-toggle" href="#" id="pagesMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ color: 'white' }}>Pages</a>
-                      <div className="dropdown-menu" aria-labelledby="pagesMenu">
-                        <a className="dropdown-item" href="blog.html">Blog</a>
-                        <a className="dropdown-item" href="login.html">Login</a>
-                        <a className="dropdown-item" href="register.html">Register</a>
-                        <a className="dropdown-item" href="faq.html">FAQ</a>
-                        <a className="dropdown-item" href="404.html">404</a>
-                        <a className="dropdown-item" href="careers.html">Careers</a>
-                        <a className="dropdown-item" href="blog-single.html">Single blog</a>
-                        <a className="dropdown-item" href="privacy-policy.html">Privacy policy</a>
+                      <a className="nav-link dropdown-toggle" href="#" id="productMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >Product <i class="fas fa-caret-down" style={{ marginLeft: '5%' }}></i></a>
+                      <div className="dropdown-menu" aria-labelledby="productMenu">
+                        <a className="dropdown-item" href="blog.html">Radio Channel</a>
+                        <a className="dropdown-item" href="login.html">Digital Advertising</a>
+                        <a className="dropdown-item" href="register.html">Application Product</a>
                       </div>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="contact.html" style={{ color: 'white' }}>Contact</a>
+                      <a className="nav-link" href="features.html" >Career</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="contact.html" >About Us</a>
+                    </li>
+                  </ul>
+                  <ul className="navbar-nav mt-2 mt-lg-0">
+                    <li class="nav-item">
+                      <a class="btn btn-secondary" href="login.html">Contact Us</a>
                     </li>
                   </ul>
                 </div>
--- src/Screens/HeaderContent.jsx
+++ src/Screens/HeaderContent.jsx
@@ -3,20 +3,62 @@ import React from 'react';
 const HeaderContent = () => {
     return (
         <>
-            <div className="row">
-                <div className="col-md-6">
-                    <h1>Attendance does not need to be complicated with this apps</h1>
-                    <p className="text-dark">He has led a remarkable campaign, defying the traditional mainstream parties courtesy of his En Marche! movement.</p>
-                    <button className="btn btn-danger mb-4">Read More!</button>
-                    <div className="my-2">
-                        <p className="header-app-download-title">GET OUR MOBILE APP</p>
+            <div id="carouselExampleInterval" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-interval={5000}>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <h1>Attendance does not need to be complicated with this apps.</h1>
+                                <p className="text-dark">He has led a remarkable campaign, defying the traditional mainstream parties courtesy of his En Marche! movement.</p>
+                                <button className="btn btn-danger mb-4">Read More!</button>
+                                <div className="my-2">
+                                    <p className="header-app-download-title">GET OUR MOBILE APP</p>
+                                </div>
+                                <div>
+                                    <button className="btn btn-app-download"><img src="images/android.svg" alt="play store" /></button>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <img src="images/Group 21.png" alt="app" width="388px" className="img-fluid" />
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <button className="btn btn-app-download"><img src="images/android.svg" alt="play store" /></button>
+                    <div className="carousel-item" data-interval={5000}>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <h1>A peer to peer chat application that allows you to communicate between individuals.</h1>
+                                <p className="text-dark">Secure chat application, with Peer to Peer technology, messages and data are sent directly between users, without being stored on the server All Control is in your hands.</p>
+                                <button className="btn btn-danger mb-4">Read More!</button>
+                                <div className="my-2">
+                                    <p className="header-app-download-title">GET OUR MOBILE APP</p>
+                                </div>
+                                <div>
+                                    <button className="btn btn-app-download"><img src="images/android.svg" alt="play store" /></button>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <img src="images/Group 222.png" alt="app" width="388px" className="img-fluid" />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-md-6">
-                    <img src="images/Group 21.png" alt="app" width="388px" className="img-fluid" />
+                    <div className="carousel-item" data-interval={5000}>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <h1>X-Channel Radio. Set the Best Music Radio.</h1>
+                                <p className="text-dark">The best X-channel Set Radio Music. serving you with the newest and the best music around 24 hours a day, and 7 days a week.</p>
+                                <button className="btn btn-danger mb-4">Read More!</button>
+                                <div className="my-2">
+                                    <p className="header-app-download-title">GET OUR MOBILE APP</p>
+                                </div>
+                                <div>
+                                    <button className="btn btn-app-download"><img src="images/android.svg" alt="play store" /></button>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <img src="images/Group 231.png" alt="app" width="388px" className="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
